a9b75f3c608f9827b243049f882270adcf795842
tsoudibi <47566877+tsoudibi@users.noreply.github.com>
2020-02-26 10:56:59 +0800

    Update HSV.ino

--- Code/Chapter 1/Samples/HSV/HSV.ino
+++ Code/Chapter 1/Samples/HSV/HSV.ino
@@ -1,6 +1,9 @@
 #include "HSV.h"
 
 void setup() {
+  int redPin = 11;
+  int greenPin = 10;
+  int bluePin = 9;
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
   pinMode(bluePin, OUTPUT);
@@ -10,8 +13,8 @@ void loop() {
   hue=360;      //H值 0~360度色盤
   saturation=1; //S值 飽和度
   value=1;      //V值 明度
-  setColor( hue, saturation, value);
-  /* //呼吸燈
+  setColor( hue, saturation, value); //使用此行程式碼來更改顏色
+  /* 彩色更改範例
   delay(50);
   hue += 10;
   if (hue >= 360) hue = 0.0;*/
