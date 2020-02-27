de0bc009ef1ef240ed80ec8c53ff89f91f0eed34
hbr890627 <hbr890627@gmail.com>
2020-02-28 01:51:49 +0800

    update

--- Code/Bluetooth/Bluetooth.ino
+++ Code/Bluetooth/Bluetooth.ino
@@ -1,6 +1,6 @@
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTSerial(0,1); //RX/TX
+SoftwareSerial BTSerial(10,11); //RX/TX
 
 int analogPin=14;
 
@@ -25,6 +25,9 @@ void setup() {
 }
 
 void loop(){
+  if(Serial.available()){
+    BTSerial.write(Serial.read());
+  }
   if(BTSerial.available()>0){ //BTSerial
     a=BTSerial.read();
     b+=a;
